430d20b571ff4117cd35ca4c4d6c68d2d2c2b9d9
Cat-Hanbit <gaulsisun501@gmail.com>
2021-12-01 23:55:55 +0900

    2021-12-01 : study12 calendar 보완

--- javaScript_jQuery/src/main/webapp/jQuery/study12/app.js
+++ javaScript_jQuery/src/main/webapp/jQuery/study12/app.js
@@ -1,76 +1,104 @@
-$(function() {
-	makeCal($("#calendar_wrap"), new Date());	
+$(function() {	
+	makeCalendar($("#calendar_wrap"), new Date());
 });
 
-function makeCal(target, date) {
+//달력 Table 포맷 그리기
+function makeHtml(year, month) {
+	var calendar_html_code =
+	    "<table class='custom_calendar_table'>" +
+	    "<thead class='cal_yyyymm'>" +
+	    "<th colspan='7'><p><span>" + year + "</span>년 <span>" + month + "</span>월</p></th>" +
+	    "</thead>" +
+	    "<thead  class='cal_week'>" +
+	    "<th>일</th><th>월</th><th>화</th><th>수</th><th>목</th><th>금</th><th>토</th>" +
+	    "</thead>" +
+	    "<tbody id='custom_set_date'>" +
+	    "</tbody>" +
+	    "<tfoot class='cal_move'>" +
+	    "<th colspan='7'><p><span class='prev'>이전 달</span>&nbsp&nbsp<span class='next'>다음 달</span></p></th>" +
+	    "</tfoot>" +
+	    "</table>";
+	return calendar_html_code;
+}
 
-	//처음 실행시 현재날짜 정보
+//달력 그리기
+function makeCalendar(target, date) {
+
+	//---------------------------------------------------------------------------------------
+	//매개변수 예외처리 
+	if (target == null || target == undefined) {
+		target = $("#calendar_wrap");
+    }
+	
     if (date == null || date == undefined) {
-        date = new Date();
+        date = new Date();	//default값-현재년월
     }
 
-	//2021년 11월
+	//---------------------------------------------------------------------------------------
     var selDate = date;
     var year = selDate.getFullYear();
     var month = selDate.getMonth() + 1;
+    
+    //달력 Table 포맷 그리기 Call
     $(target).empty().append(makeHtml(year, month));
 	
-	//1일의 요일 계산
-    var thisMonth = new Date(selDate.getFullYear(), selDate.getMonth(), 1);
-	//마지막 일자 계산
-    var thisLastDay = new Date(selDate.getFullYear(), selDate.getMonth() + 1, 0);
+	//---------------------------------------------------------------------------------------
+	//일자 입력
+    var thisFirstDay = new Date(selDate.getFullYear(), selDate.getMonth(), 1);		//12월 1일
+    var thisLastDay = new Date(selDate.getFullYear(), selDate.getMonth() + 1, 0);	//12월 31일
 
-	
-    var tag = "<tr>";
-    var cnt = 0;
+    var tag = "";
+    var cnt = 0;	//0:일, 1:월, 2:화, 3:수, 4:목, 5:금, 6:토, 7:0으로 리셋
     
-	//빈 공백 tag
-    for (i = 0; i < thisMonth.getDay(); i++) {
+	//앞 빈 공백 tag
+    for (i = 0; i < thisFirstDay.getDay(); i++) {
+        if (cnt % 7 == 0) { 
+        	tag += "<tr>"; 
+        	cnt = 0;
+       	}
+        
         tag += "<td></td>";
         cnt++;
     }
 
     //일자 tag
     for (i = 1; i <= thisLastDay.getDate(); i++) {
-        if (cnt % 7 == 0) { tag += "<tr>"; }
+        if (cnt % 7 == 0) { 
+        	tag += "<tr>"; 
+        	cnt = 0;
+       	}
 
         tag += "<td>" + i + "</td>";
         cnt++;
+        
         if (cnt % 7 == 0) {
             tag += "</tr>";
         }
     }
-    $(target).find("#custom_set_date").append(tag);
-    clickEvent();
 
-    function makeHtml(year, month) {
-        var calendar_html_code =
-            "<table class='custom_calendar_table'>" +
-            "<thead class='cal_yyymm'>" +
-            "<th colspan='7'><p><span>" + year + "</span>년 <span>" + month + "</span>월</p></th>" +
-            "</thead>" +
-            "<thead  class='cal_week'>" +
-            "<th>일</th><th>월</th><th>화</th><th>수</th><th>목</th><th>금</th><th>토</th>" +
-            "</thead>" +
-            "<tbody id='custom_set_date'>" +
-            "</tbody>" +
-            "<tfoot class='cal_move'>" +
-            "<th colspan='7'><p><span class='prev'>이전 달</span>&nbsp&nbsp<span class='next'>다음 달</span></p></th>" +
-            "</tfoot>" +
-            "</table>";
-        return calendar_html_code;
+	//뒤 빈 공백 tag
+    for (i = thisLastDay.getDay()+1; i < 7; i++) {
+        tag += "<td></td>";
+        cnt++;
+        
+        if (cnt % 7 == 0) {
+            tag += "</tr>";
+        }
     }
+    
+    //일자 append
+    $(target).find("#custom_set_date").append(tag);
+
+	//---------------------------------------------------------------------------------------
+	//수행버튼 이전달 클릭이벤트
+	$(".custom_calendar_table").on("click", ".prev", function () {
+	    selDate = new Date(selDate.getFullYear(), selDate.getMonth() - 1, selDate.getDate());
+	    makeCalendar($(target), selDate);
+	});
 	
-	function clickEvent() {
-        //이전달
-        $(".custom_calendar_table").on("click", ".prev", function () {
-            selDate = new Date(selDate.getFullYear(), selDate.getMonth() - 1, selDate.getDate());
-            makeCal($(target), selDate);
-        });
-        //다음날
-        $(".custom_calendar_table").on("click", ".next", function () {
-            selDate = new Date(selDate.getFullYear(), selDate.getMonth() + 1, selDate.getDate());
-            makeCal($(target), selDate);
-        });
-     }
+	//수행버튼 다음날 클릭이벤트
+	$(".custom_calendar_table").on("click", ".next", function () {
+	    selDate = new Date(selDate.getFullYear(), selDate.getMonth() + 1, selDate.getDate());
+	    makeCalendar($(target), selDate);
+	});
 }
